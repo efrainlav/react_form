b479afe7b5051c32559d30ded0b17bcda01de934
efrainlav <efrainlav@gmail.com>
2023-11-25 11:11:39 -0500

    React-Form-Elm: Creacion del componente SigIn ELM

---
+++ react-forms-authentication/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import ForgotPassword from "./components/ForgotPassword";
+
+const App = () => {
+  const [step, setStep] = useState("signin");
+
+  return(
+<div className="container">
+  {step === "signin" && <SignIn />}
+  {step === "signup" && <SignUp />}
+  {step === "forgot" && <ForgotPassword/>}
+</div>
+  
+  );
+};
+
+export default App;
---
+++ react-forms-authentication/src/components/SignIn.jsx
@@ -0,0 +1,27 @@
+const SignIn = () => {
+  return (
+    <main>
+      <h3>Sign In</h3>
+      <div className="card">
+        <form autoComplete="off">
+          <fieldset>
+            <label htmlFor="email">email</label>
+            <input type="email" id="email" autoFocus />
+            <p className="error">Valid email required</p>
+          </fieldset>
+          <fieldset>
+            <label htmlFor="password">password</label>
+            <input type="password" id="password" />
+            <p className="error">Password is required</p>
+          </fieldset>
+          <button type="submit">login</button>
+          <div className="forgot">Forgot password?</div>
+        </form>
+      </div>
+      <p>Don't have an account? <span>Sign up</span>
+      </p>
+    </main>
+  );
+};
+
+export default SignIn
