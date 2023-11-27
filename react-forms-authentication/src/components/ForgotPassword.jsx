ae3a91fccf0588eb698a66ab95acf0c2936eaae6
efrainlav <efrainlav@gmail.com>
2023-11-26 20:43:17 -0500

    React-Form-Elm: Creacion del componente ForgotPassword ELM

--- react-forms-authentication/src/components/ForgotPassword.jsx
+++ react-forms-authentication/src/components/ForgotPassword.jsx
@@ -1,7 +1,20 @@
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <main>
+      <h3>Forgot Password</h3>
+      <div className="card">
+        <form action="">
+          <fieldset>
+            <label htmlFor="email">email</label>
+            <input type="email" id="email" autoFocus />
+            <p className="error">Valid email required</p>
+          </fieldset>
+          <button type="submit">Remember me!</button>
+        </form>
+      </div>
+      <p>Already have an acount? <span>Sign In</span> </p>
+    </main>
+  );
+};
 
 export default ForgotPassword
