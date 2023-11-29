cb969377f89d64a271db3c818254a4503e700d29
efrainlav <efrainlav@gmail.com>
2023-11-29 11:17:37 -0500

    React Forms: Validación manual de inputs de un formulario - ELM

--- react-forms-authentication/src/components/ForgotPassword.jsx
+++ react-forms-authentication/src/components/ForgotPassword.jsx
@@ -3,12 +3,14 @@ import AuthContext from "../context/AuthContext";
 
 const ForgotPassword = () => {
   let { setStep } = useContext(AuthContext);
+
+
   return (
     <main>
       <h3>Forgot Password</h3>
       <div className="card">
         <form action="">
-          <fieldset>
+        <fieldset>
             <label htmlFor="email">email</label>
             <input type="email" id="email" autoFocus />
             <p className="error">Valid email required</p>
@@ -16,9 +18,12 @@ const ForgotPassword = () => {
           <button type="submit">Remember me!</button>
         </form>
       </div>
-      <p>Already have an acount? <span onClick={() => setStep("signin")}>Sign In</span> </p>
+      <p>
+        Already have an acount?{" "}
+        <span onClick={() => setStep("signin")}>Sign In</span>{" "}
+      </p>
     </main>
   );
 };
 
-export default ForgotPassword
+export default ForgotPassword;
--- react-forms-authentication/src/components/SignIn.jsx
+++ react-forms-authentication/src/components/SignIn.jsx
@@ -1,6 +1,5 @@
 import { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
-import InputPassword from "./InputPassword";
 
 const SignIn = () => {
   let { setStep } = useContext(AuthContext);
@@ -8,6 +7,10 @@ const SignIn = () => {
   //Estado del formulario
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  
+  // Focus de los inputs del formulario
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [passwordFocused, setPasswordFocused] = useState(false);
 
   const handlerSubmit = (e) => {
     //evitar el envio de datos a un Backend
@@ -30,6 +33,9 @@ const SignIn = () => {
               onChange={(e) => setEmail(e.target.value)}
               id="email"
               autoFocus
+              required
+              onBlur={() => setEmailFocused(true)}
+              data-focused={emailFocused}
             />
             <p className="error">Valid email required</p>
           </fieldset>
@@ -41,8 +47,12 @@ const SignIn = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               id="password"
+              pattern="(?=.*[\W])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              required
+              onBlur={() => setPasswordFocused(true)}
+              data-focused={passwordFocused}
             />
-            <p className="error">Password is required </p>
+            <p className="error">Valid password required</p>
           </fieldset>
           <button type="submit">login</button>
           <div className="forgot" onClick={() => setStep("forgot")}>
