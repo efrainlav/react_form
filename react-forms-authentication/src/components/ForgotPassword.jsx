8631ababb68757d77875d78f3c3c51eb4c7c04d5
efrainlav <efrainlav@gmail.com>
2023-11-29 14:58:33 -0500

    React Forms: Validación de formulario ForgotPassword con Formik usando hooks: ELM

--- react-forms-authentication/src/components/ForgotPassword.jsx
+++ react-forms-authentication/src/components/ForgotPassword.jsx
@@ -1,19 +1,39 @@
+import { useFormik } from "formik";
 import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
+import { forgotPasswordSchema } from "../schemas/forgotPasswordSchema";
 
 const ForgotPassword = () => {
   let { setStep } = useContext(AuthContext);
+  const {values, handleChange, handleBlur, handleSubmit, errors } = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: forgotPasswordSchema,
+    onSubmit,
+  });
 
+  function onSubmit(){
+    console.log(values);
+  };
 
   return (
     <main>
       <h3>Forgot Password</h3>
       <div className="card">
-        <form action="">
-          <fieldset>
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <fieldset><small>{errors.email} </small>
             <label htmlFor="email">email</label>
-            <input type="email" id="email" autoFocus />
-            <p className="error">Valid email required</p>
+            <input
+              type="email"
+              id="email"
+              autoFocus
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {/*  */}
+            {errors.email && <p className="error-message">{errors.email}</p> }
           </fieldset>
           <button type="submit">Remember me!</button>
         </form>
